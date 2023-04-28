57ea97e4972085743f90ee4138b8ff367fe3a743
Punit Ramaji <punitramaji@users.noreply.github.com>
2023-04-28 11:54:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,8 +14,6 @@ terraform {
 module "s3_artifacts_bucket" {
   source                = "./modules/s3"
   project_name          = var.project_name
-  kms_key_arn           = module.codepipeline_kms.arn
-  codepipeline_role_arn = module.codepipeline_iam_role.role_arn
   tags = {
     Project_Name = var.project_name
     Environment  = var.environment
