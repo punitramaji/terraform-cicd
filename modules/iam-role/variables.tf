bee4d2c49a03daec46772becdfeb7ed80837a807
Punit Ramaji <punitramaji@users.noreply.github.com>
2023-04-28 19:35:40 +0530

    Create variables.tf

---
+++ modules/iam-role/variables.tf
@@ -0,0 +1,20 @@
+variable "create_new_role" {
+  type        = bool
+  description = "Flag for deciding if a new role needs to be created"
+  default     = true
+}
+
+variable "codepipeline_iam_role_name" {
+  description = "Name of the IAM role to be used by the project"
+  type        = string
+}
+
+variable "project_name" {
+  description = "Unique name for this project"
+  type        = string
+}
+
+variable "s3_bucket_arn" {
+  description = "The ARN of the S3 Bucket"
+  type        = string
+}
