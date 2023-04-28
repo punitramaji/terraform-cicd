1e1ed2fbf4bde9599e47fa85389cdeef3d42490d
Punit Ramaji <punitramaji@users.noreply.github.com>
2023-04-28 18:28:39 +0530

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -4,19 +4,6 @@ resource "aws_s3_bucket" "codepipeline_bucket" {
   force_destroy = true
 }
 
-resource "aws_s3_bucket_public_access_block" "codepipeline_bucket_access" {
-  bucket                  = aws_s3_bucket.codepipeline_bucket.id
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-  block_public_acls       = true
-  block_public_policy     = true
-}
-
-resource "aws_s3_bucket_acl" "codepipeline_bucket_acl" {
-  bucket = aws_s3_bucket.codepipeline_bucket.id
-  acl    = "private"
-}
-
 resource "aws_s3_bucket_versioning" "codepipeline_bucket_versioning" {
   bucket = aws_s3_bucket.codepipeline_bucket.id
   versioning_configuration {
