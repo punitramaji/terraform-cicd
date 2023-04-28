9e0b6a6129aeb53f0d9b7a38ac4e768da235b7b0
Punit Ramaji <punitramaji@users.noreply.github.com>
2023-04-28 11:55:19 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,13 +6,3 @@ variable "tags" {
   description = "Tags to be associated with the S3 bucket"
   type        = map(any)
 }
-
-variable "kms_key_arn" {
-  description = "ARN of KMS key for encryption"
-  type        = string
-}
-
-variable "codepipeline_role_arn" {
-  description = "ARN of the codepipeline IAM role"
-  type        = string
-}
