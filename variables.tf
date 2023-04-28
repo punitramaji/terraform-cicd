ec63ff15b6beb9e8e2b3dd99a8d00d5bd4773303
Punit Ramaji <punitramaji@users.noreply.github.com>
2023-04-28 11:58:15 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,7 +2,3 @@ variable "project_name" {
   description = "Name of the project to be prefixed to create the s3 bucket"
   type        = string
 }
-variable "tags" {
-  description = "Tags to be associated with the S3 bucket"
-  type        = map(any)
-}
