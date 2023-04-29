fe17ce6ae98f87110fa6e8e85da1304ad62a00f8
Punit Ramaji <punitramaji@users.noreply.github.com>
2023-04-30 02:35:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,3 +27,18 @@ module "codepipeline_iam_role" {
   codepipeline_iam_role_name = var.create_new_role == true ? "${var.project_name}-codepipeline-role" : var.codepipeline_iam_role_name
   s3_bucket_arn              = module.s3_artifacts_bucket.arn
 }
+
+  module "codebuild_terraform" {
+  source = "./modules/codebuild"
+
+  project_name                        = var.project_name
+  role_arn                            = module.codepipeline_iam_role.role_arn
+  s3_bucket_name                      = module.s3_artifacts_bucket.bucket
+  build_projects                      = var.build_projects
+  build_project_source                = var.build_project_source
+  builder_compute_type                = var.builder_compute_type
+  builder_image                       = var.builder_image
+  builder_image_pull_credentials_type = var.builder_image_pull_credentials_type
+  builder_type                        = var.builder_type
+  kms_key_arn                         = module.codepipeline_kms.arn
+}
